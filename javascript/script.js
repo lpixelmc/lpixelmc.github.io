9b0d1eac3d6879f598008e6dd1849f8e17770233
Incbom <99501939+IncbomDev@users.noreply.github.com>
2023-02-05 21:13:36 -0500

    homepage changes
    Co-authored-by: rohanmishra <rohanmishr@users.noreply.github.com>

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,10 @@
 var cover = document.getElementById("cover");
 var coverIndex = 0;
-
+var galleryIndex = 0;
 window.addEventListener('load', function(){
     document.querySelector('html').style.visibility = "visible";
 },1);
+
 setInterval(function(){
     if(coverIndex === 0){
         coverIndex++;
@@ -18,4 +19,15 @@ setInterval(function(){
         coverIndex = 0;
         cover.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1020450091192041482/1070474809022099466/bg3.png')";
     }
-},5000);
+
+    if(galleryIndex === 0){
+        galleryIndex++;
+        document.getElementById("galleryInner").style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1063862971710836866/1071969189914492928/2023-02-05_18.49.05.png')";
+    }else if (galleryIndex == 1){
+        galleryIndex++;
+        document.getElementById("galleryInner").style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1063862971710836866/1071969201918582784/2023-02-05_18.52.38.png')";
+    }else if(galleryIndex == 2){
+        galleryIndex = 0;
+        document.getElementById("galleryInner").style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1063862971710836866/1071974138715050095/2023-02-05_21.02.13.png')";
+    }
+}, 5000);
